890bc25c594328b740161ae02b62d35de3664df4
goal-dev-kzw <kzw.purpose.dev.20@gmail.com>
2022-05-30 22:11:09 +0630

    make u

--- images/index.js
+++ images/index.js
@@ -2,3 +2,22 @@ $(window).on("load",function () {
 	$('#status').fadeOut(1000);
 	$('#preloader').delay(1100).fadeOut(1000);
 })
+
+
+$(document).ready(function () {
+	$(".owl-carousel").owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		dots:false,
+		autoplay:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			1000:{
+				items:2
+			}
+		}
+	})
+})
